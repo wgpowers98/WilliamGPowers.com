fa433c0a75182e0b531b0c6e1429fa3c2588bc89
Will Powers <wgpowers98@yahoo.com>
2022-12-31 17:53:38 -0500

    Updates

---
+++ public/Scripts/Home.js
@@ -0,0 +1,14 @@
+
+function neonScriptEnter ()
+{
+    SignJS = document.getElementById("neonListen");
+    SignJS.classList.add("Neon");
+    
+}
+
+function neonScriptLeave() 
+{
+    SignJS = document.getElementById("neonListen");
+    SignJS.classList.remove("Neon");
+}
+
