1c3c460344798a3fa675797c80d61dd892dabbed
Will Powers <wgpowers98@yahoo.com>
2023-09-06 21:43:11 -0400

    Page Overhaul

--- public/Scripts/Home.js
+++ public/Scripts/Home.js
@@ -12,26 +12,6 @@ function neonScriptLeave()
     SignJS.classList.remove("Neon");
 }
 
-function showCoords(event) 
-{
-    if (blockMove)
-    {
-        var x = event.clientX;
-        var y = event.clientY;
-        M = document.getElementById("FollowM");
-        M.style.paddingLeft = x-90+"px";
-        M.style.paddingTop = y-495+"px";
-    }
-}
 
-function movable()
-{
-    if (blockMove)
-    {
-        blockMove = false;
-    } else 
-    {
-        blockMove = true;
-    }
-}
+
 
